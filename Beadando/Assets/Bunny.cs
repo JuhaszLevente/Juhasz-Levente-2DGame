24c2fff4323309d35ca8cf897762196d796b4d5e
JuhaszLevente <yuhi.levi@gmail.com>
2020-12-20 15:21:58 +0100

    Nyuszi Destroy
    A nyulak meghalnak ha a piros négyzet a collidereükben van és az A gomb le van nyomva.

--- Beadando/Assets/Bunny.cs
+++ Beadando/Assets/Bunny.cs
@@ -2,40 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Bunny : MonoBehaviour
 {
     public float speed = 5f;
 
-    private bool can_move = true;
+
 
     public float bound_X = -20f;
-    // Start is called before the first frame update
+
    
 
     // Update is called once per frame
     void Update()
     {
-       // Move();
 
     }
-   /* void Move()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (can_move)
+        if (Input.GetKey(KeyCode.A))
         {
-            Vector3 temp = transform.position;
-            temp.y += speed * Time.deltaTime;
-            transform.position = temp;
-
-            if (temp.y < bound_X)
-                gameObject.SetActive(false);
-        }
-        
-    }*/
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.name == "Player")
-        {
-            Destroy(col.gameObject);
+            if (collision.gameObject.name == "Player")
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
