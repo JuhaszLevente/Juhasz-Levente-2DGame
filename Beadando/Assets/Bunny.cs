c9c6b07f430fbe77e291358eb5549815917c1c66
JuhaszLevente <yuhi.levi@gmail.com>
2020-12-20 16:19:16 +0100

    Nyuszi Timer
    Bizonyos idő után a nyuszik letörlődnek pont hozzáadása nélkül.

--- Beadando/Assets/Bunny.cs
+++ Beadando/Assets/Bunny.cs
@@ -7,15 +7,29 @@ public class Bunny : MonoBehaviour
 {
     public float speed = 5f;
 
-
+    public float timer = 3;
 
     public float bound_X = -20f;
 
-   
 
+    
     // Update is called once per frame
     void Update()
     {
+        if(gameObject.name == "Bunny(Clone)")
+        {
+         if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+
+                Destroy(gameObject);
+
+            }
+
+        }
 
     }
     private void OnTriggerStay2D(Collider2D collision)
